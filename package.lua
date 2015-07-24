9072b5cb7572081c442d1a243069d4354d614553
lduboeuf <lionel.duboeuf@gmail.com>
2015-07-24 23:37:25 +0200

    correct version, remove deps folder

--- package.lua
+++ package.lua
@@ -5,7 +5,7 @@ return {
   dependencies = {
    	"luvit/require@1.2.0",
     "luvit/pretty-print",
-    "lduboeuf/cjson@1.0.1",
+    "lduboeuf/cjson@0.0.1",
     "luvit/json@1.0.0", --needed by weblit-static
   	"creationix/weblit-app@0.2.6",
     "creationix/weblit-auto-headers@0.1.2",
